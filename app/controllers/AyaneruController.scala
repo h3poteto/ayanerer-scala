02a29977c13a3db485c003edb268ed8e3beb7393
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2017-03-29 15:40:21 +0900

    refs #11 Add AyaneruController and registration form

--- app/controllers/AyaneruController.scala
+++ app/controllers/AyaneruController.scala
@@ -3,19 +3,32 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.libs.json._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import spray.json._
+import play.api.i18n.{MessagesApi, Messages, I18nSupport}
 import dao.AyaneruDAO
 import models.Ayaneru
-import models.AyaneruJsonProtocol._
-
 
 @Singleton
-class AyaneruController @Inject() (ayaneruDao: AyaneruDAO) extends Controller {
-  def index = Action.async {
-    ayaneruDao.all().map {
-      ayanerus => Ok(ayanerus.toJson.prettyPrint)
+class AyaneruController @Inject() (ayaneruDao: AyaneruDAO, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  val registrationForm = Form[Ayaneru](
+    mapping(
+      "id"    -> ignored[Option[Int]](None),
+      "image" -> text
+    )(Ayaneru.apply)(Ayaneru.unapply)
+  )
+
+  def registration = Action {
+    Ok(views.html.ayaneru.registration(registrationForm))
+  }
+
+  def create = Action.async { implicit request =>
+    val ayaneru: Ayaneru = registrationForm.bindFromRequest.get
+    ayaneruDao.insert(ayaneru).map { _ =>
+      Redirect(routes.HomeController.index)
     }
   }
 }
+

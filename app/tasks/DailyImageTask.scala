ca74ffa0df48883549a4a8275cf91eb05e7a6843
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2017-04-10 14:49:54 +0900

    refs #36 Add comment which describe how to run a task in task class

--- app/tasks/DailyImageTask.scala
+++ app/tasks/DailyImageTask.scala
@@ -4,6 +4,14 @@ import play.api._
 import models.{ GoogleSearchResultDownloader, DailySearchRequest }
 import akka.actor._
 
+// How to run this task:
+// in development:
+// $ activator "run-main tasks.DailyImageTask"
+// in production:
+// $ cd ayanerer-1.0-SNAPSHOT
+// $ bin/ayanerer -main tasks.DailyImageTask -Dconfig.file=./conf/prod.conf
+
+
 object DailyImageTask extends App with Task {
   def task(app: Application) = {
     getImages(app, "佐倉綾音")
